a3d16cd8dd83db28ec1d74fca111ef2bbc552056
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-02-21 14:06:24 +0500

    Transition Table

--- Phase 1/Lexer.cpp
+++ Phase 1/Lexer.cpp
@@ -7,11 +7,34 @@ using namespace std;
 
 vector<vector<int>> Transition;
 vector<vector<int>> Advance;
+vector<string> Keywords;
 
-vector<vector<int>> getTransitionTable() { return {{}}; }
+vector<vector<int>> getTransitionTable() {
+  ifstream TransitionFile("TransitionTable.txt");
+
+  if (!TransitionFile.is_open()) {
+    cout << "Error: TransitionTable.txt not found" << endl;
+    return {{}};
+  }
+
+  TransitionFile.close();
+  return {{}};
+}
 
 vector<vector<int>> getAdvanceTable(vector<vector<int>> &Transition) {
   return {{}};
+}
+
+vector<string> getKeywords() {
+  ifstream KeywordsFile("Keywords.txt");
+
+  if (!KeywordsFile.is_open()) {
+    cout << "Error: Keywords.txt not found" << endl;
+    return {};
+  }
+
+  KeywordsFile.close();
+  return {};
 }
 
 int main() { return 0; }
