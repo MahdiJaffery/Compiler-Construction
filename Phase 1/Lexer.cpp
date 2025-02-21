e82e49c1a678db4a299001464cf7d228b066fc28
Mahdi Jaffery <mahdijaffri5@gmail.com>
2025-02-21 13:49:32 +0500

    Keywords

---
+++ Phase 1/Lexer.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> Transition;
+vector<vector<int>> Advance;
+
+vector<vector<int>> getTransitionTable() { return {{}}; }
+
+vector<vector<int>> getAdvanceTable(vector<vector<int>> &Transition) {
+  return {{}};
+}
+
+int main() { return 0; }
